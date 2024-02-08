1e0af478fe0d73eeeea1739401cca10c96f347cb
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-08 20:31:48 -0300

    feat: add absolute path and colors

--- src/navigation/tab/navigation.tsx
+++ src/navigation/tab/navigation.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {TabNavigationProps} from './types';
-import {Home, Settings} from '../../screens';
+import {Home, Settings} from 'src/screens';
+import colors from 'src/styles/colors';
 
 const Tab = createBottomTabNavigator<TabNavigationProps>();
 
+type iconProps = {
+  name: string;
+  focused: boolean;
+};
+
 const TabNavigator = () => {
+  const iconTab = ({focused, name}: iconProps) => {
+    const color = focused ? colors.primary : colors.grey;
+    return <Icon name={name} size={25} color={color} />;
+  };
+
   return (
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => iconTab({name: 'home', focused}),
+          tabBarActiveTintColor: colors.primary,
+          tabBarInactiveTintColor: colors.grey,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({focused}) => iconTab({name: 'settings', focused}),
+          tabBarActiveTintColor: colors.primary,
+          tabBarInactiveTintColor: colors.grey,
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ src/styles/colors.ts
@@ -0,0 +1,6 @@
+export default {
+  primary: '#366ED8',
+  secondary: '#F2F3F3',
+  error: '#F3A953',
+  grey: '#BDBDBD',
+};
