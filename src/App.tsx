7f853f11c6e35f827b012756c913f808d9f09aaa
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-09 09:58:43 -0300

    feat: add gesture handle and reanimated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import {LogBox} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from './navigation/tab/navigation';
 
+LogBox.ignoreLogs([
+  'RCTBridge required dispatch_sync to load RCTAccessibilityManager. This may lead to deadlocks',
+]);
+
 const App = () => {
+  const gestureStyle = {flex: 1};
+
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <GestureHandlerRootView style={gestureStyle}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
