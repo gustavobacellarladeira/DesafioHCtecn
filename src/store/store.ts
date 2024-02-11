ae06d2870789df533f5277cc1d6359333414d881
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-11 13:25:00 -0300

    feat: redux store add formReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import homeReducer from './features/home/slice';
+import formReducer from './features/form/slice';
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    form: formReducer,
   },
 });
 
