e66e0484e183f6ce50d91663d4bd03b0439da025
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-11 23:48:08 -0300

    feat: redux form

--- src/store/features/form/slice.ts
+++ src/store/features/form/slice.ts
@@ -1,5 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {createTask} from './thunk';
+import {createTask, loadTask, updateTask} from './thunk';
 import {ActionCompleted, State} from './types';
 
 const INITIAL_STATE: State = {
@@ -23,12 +23,24 @@ export const slice = createSlice({
       task.description = '';
       task.category = '';
     });
+    builder.addCase(loadTask.fulfilled, (state, action) => {
+      state.task = action.payload as State['task'];
+    });
+    builder.addCase(updateTask.fulfilled, state => {
+      const {task} = state;
+      state.actionCompleted = ActionCompleted.UPDATED;
+      task.name = '';
+      task.description = '';
+      task.category = '';
+    });
   },
 });
 
 export const formActions = {
   ...slice.actions,
   createTask,
+  loadTask,
+  updateTask,
 };
 
 export default slice.reducer;
--- src/store/features/form/thunk.ts
+++ src/store/features/form/thunk.ts
@@ -1,7 +1,29 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-import {createTask as create} from 'src/services/firestore';
+import {
+  createTask as create,
+  loadTask as load,
+  updateTask as update,
+} from 'src/services/firestore';
 import {Task} from './types';
 
-export const createTask = createAsyncThunk('form/createTask', (task: Task) => {
-  create(task);
-});
+export const createTask = createAsyncThunk(
+  'form/createTask',
+  async (task: Task) => {
+    await create(task);
+  },
+);
+
+export const loadTask = createAsyncThunk(
+  'form/loadTask',
+  async (id: string) => {
+    const res = await load(id);
+    return res;
+  },
+);
+
+export const updateTask = createAsyncThunk(
+  'form/updateTask',
+  async (task: {id: string; task: Task}) => {
+    await update(task.id, task.task);
+  },
+);
